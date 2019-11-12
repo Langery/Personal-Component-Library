9de45e740439868dae882b7c145009bdfd1c9f05
Langery <1245066197@qq.com>
2019-11-12 15:43:03 +0800

    change the files

--- common/timeDeal.js
+++ common/timeDeal.js
@@ -1,5 +1,5 @@
-function timeDeal () {
-  let dateNow = new Date()
+function timeDeal (data) {
+  let dateNow = data === undefined ? new Date() : new Date(data)
   let year = dateNow.getFullYear()
   let month = dateNow.getMonth() + 1
   let date = dateNow.getDate()
@@ -11,20 +11,6 @@ function timeDeal () {
     let fullDate = date < 10 ? `0${date}` : date
     return fullDate
   }
-
-  function handleMillSec(date) {
-    let fullDate = date >= 100 ? date : date >= 10 ? `0${date}` : `00${date}`
-  }
-  return `${year}-${handleTime(month)}-${handleTime(date)} ${handleTime(hour)}:${handleTime(min)}:${handleTime(sec)}`
-  // var theTime = new Date().getTime()
-  // var beforTime = theTime - (1 * 3600 * 24 * 1000 * 7)
-  // var oldTime = new Date(beforTime)
-  // let oldyear = oldTime.getFullYear()
-  // let oldmonth = oldTime.getMonth() + 1
-  // let olddate = oldTime.getDate()
-  // let oldhour = oldTime.getHours()
-  // let oldmin = oldTime.getMinutes()
-  // let oldsec = oldTime.getSeconds()
-  // this.startTime = `${oldyear}-${handleTime(oldmonth)}-${handleTime(olddate)} ${handleTime(oldhour)}:${handleTime(oldmin)}:${handleTime(oldsec)}`
+  return `${year}-${handleTime(month)}-${handleTime(date)} ${handleTime(hour)}%3A${handleTime(min)}%3A${handleTime(sec)}`
 }
 
