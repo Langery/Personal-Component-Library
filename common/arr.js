e046fee46b582a14c7080db4b0a1f0066b4f18f2
Langery <1245066197@qq.com>
2020-10-18 11:44:15 +0800

    merge

--- common/arr.js
+++ common/arr.js
@@ -7,3 +7,15 @@
 function dealArr (x, arr) {
   if (arr.includes(x)) console.log('x is had in arr;');
 }
+
+
+/**
+ * 场景描述：判断是否是数组
+ */
+function judgeArr (arr) {
+  if (Array.isArray(arr)) {
+    return true;
+  } else {
+    return false;
+  }
+}
