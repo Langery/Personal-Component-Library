7e51698af2ef20eb316f305da061af5cc7694c2b
Langery <1245066197@qq.com>
2020-10-29 10:25:39 +0800

    deal mid num

--- common/Number.js
+++ common/Number.js
@@ -9,3 +9,16 @@ function dealNum (x) {
         console.log("x is not a number");
     }
 }
+
+/**
+ * 场景描述：知道首位数字获取中间数字
+ *          只针对数字
+ */
+function midNum (a, b) {
+    var count = b - a - 1;
+    var arr = [a,b];
+    for (var i = 1;i <= count;i++) {
+        arr.push(a + i);
+    }
+    return arr;
+}
